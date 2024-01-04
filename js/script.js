3c2f76ce5645e86f179ebc98cb47404dbaa1a29b
Emilia RAFFALLI <e.raffalli@hotmail.fr>
2024-01-04 17:59:33 +0100

    Portfolio en cours

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// function toggleMenu() {
+//     var menu = document.querySelector('.menu');
+//     var menuIcon = document.querySelector('.menu-icon');
+
+//     menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
+//     menuIcon.style.display = (menuIcon.style.display === 'block') ? 'none' : 'block';
+// }
+
+
+// const menuElement = document.querySelector(".menu-icon");
+
+// menuElement.addEventListener("click", function() {
+//     this.classList.toggle("cross");
+// });
+
+// console.log('hello');
+
+document.getElementById("logo").addEventListener("click", function() {
+    this.classList.toggle("rotate");
+});
